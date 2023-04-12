3e18cdcc188d1e14aecf8f036b44839efae96e5a
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-04-12 11:28:21 +0600

    09 Context + useReducer simple project - part 2

--- src/Context/ProductProvider.js
+++ src/Context/ProductProvider.js
@@ -1,34 +1,47 @@
 import React, {
-    createContext,
-    useContext,
-    useEffect,
-    useState,
-  } from "react";
-  const PRODUCT_CONTEXT = createContext();
-  
-  const ProductProvider = ({ children }) => {
-      const [data, setData] = useState([]);
-  
-    useEffect(() => {
-      fetch("http://localhost:5000/products")
-        .then((res) => res.json())
-        .then((data) =>setData(data.data));
-    }, []);
-  
-    const value = {
-      data,
-    };
-    return (
-      <PRODUCT_CONTEXT.Provider value={value}>
-        {children}
-      </PRODUCT_CONTEXT.Provider>
-    );
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { actionType } from "../state/ProductState/actionType";
+import {
+  initialState,
+  productReducer,
+} from "../state/ProductState/productReducer";
+const PRODUCT_CONTEXT = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(productReducer, initialState);
+console.log("state",state)
+  useEffect(() => {
+    dispatch({ type: actionType.FETCHING_START });
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: actionType.FETCHING_SUCCESS, payload: data.data })
+      )
+      .catch(() => {
+        dispatch({ type: actionType.FETCHING_ERROR });
+      });
+  }, []);
+
+  const value = {
+    state,
+    dispatch
   };
-  
-  export const useProducts = () => {
-    const context = useContext(PRODUCT_CONTEXT);
-  
-    return context;
-  };
-  
-  export default ProductProvider;
+  return (
+    <PRODUCT_CONTEXT.Provider value={value}>
+      {children}
+    </PRODUCT_CONTEXT.Provider>
+  );
+};
+
+export const useProducts = () => {
+  const context = useContext(PRODUCT_CONTEXT);
+
+  return context;
+};
+
+export default ProductProvider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
 import { useProducts } from "../Context/ProductProvider";
 
 const Home = () => {
-  const { data } = useProducts();
-  console.log("home --:- ", data);
+  const {
+    state: { products },
+  } = useProducts();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      <h1>This is home page</h1>
+      {products.map(product =><ProductCard product={product}/>)}
     </div>
   );
 };
